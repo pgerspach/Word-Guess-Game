1f85e896e3966fe75f32f99b4a6476a628bc39ed
pgerspach <pgerspac@alumni.nd.edu>
2018-10-09 11:20:00 -0400

    fully functional just for 7 letter words. Needs styling too

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,15 @@ $(document).ready(function() {
   var wordArray = ["chicken", "peacock", "gorilla"];
   var gameWord = "";
   var letters = "";
-  var guessedTag = false;
   var amtRight = 0;
+  var numGuess = 0;
+  var maxGuess = 11;
+  var numWins = 0;
+  var wrongLet = "";
+  var gameStart = false;
 
-  $(".startClick").on("click", function() { // initialize a new
+  $(".startClick").on("click", function() {
+    // initialize a new
     $(".result").text("");
     amtRight = 0; // reset the amount of letters revealed
     gameWord = wordArray[Math.floor(Math.random() * wordArray.length)]; // randomly grab word from word array
@@ -15,49 +20,83 @@ $(document).ready(function() {
       // simply write blanks to screen
       $(".l" + String(i)).text("_");
     }
+    $(".wrongRem").text(String(maxGuess - numGuess));
+    gameStart = true;
   });
 
-  $("body").keyup(function(event) { //when a key is pressed
-    // When a key is typed
-    if (event.which > 64 && event.which < 91) {
-      // IF AN ALPHA CHARACTER
-      var keyString = String.fromCharCode(event.which).toLowerCase(); //CONVERT TO LOWERCASE
-      console.log(keyString);
-      for (var i = 0; i < letters.length; i++) {
-        //COMPARE TYPED VALUE TO LETTERS IN THE SECRET WORD
-        if (keyString === letters[i]) {
-          // has the typed value already been guessed?
-          guessedTag = true; // flag for an already guessed letter
-        }
-      }
+  $("body").keyup(function(event) {
+    if (gameStart) {
+      var inWord = false;
+      //when a key is pressed
+      if (event.which > 64 && event.which < 91) {
+        // IF AN ALPHA CHARACTER
+        var keyString = String.fromCharCode(event.which).toLowerCase(); //CONVERT TO LOWERCASE
+        console.log(keyString);
 
-      if (!guessedTag) {
-        // IF THE LETTER HAS NOT BEEN GUESSED YET
-        for (var i = 0; i < gameWord.length; i++) {
-          // loop through the secret word
-          if (keyString === gameWord[i]) {
-            // if typed key matches any value, do...
-            console.log("Word letter: " + gameWord[i]);
-            console.log("Key: " + keyString);
+        if (!inArray(keyString, letters)) {
+          // IF THE LETTER HAS NOT BEEN GUESSED YET
+          for (var i = 0; i < gameWord.length; i++) {
+            // loop through the secret word
+            if (keyString === gameWord[i]) {
+              // if typed key matches any value, do...
+              console.log("Word letter: " + gameWord[i]);
+              console.log("Key: " + keyString);
 
-            $(".l" + String(i)).text(keyString); // write letter to the appropriate blank space
-            console.log(".l" + String(i));
-            amtRight++; // increase the amount correct
-            if (amtRight === gameWord.length) {
-              // if the amount of revealed letters matches the length of the gameWord, you win
-              $(".result").text("You win!");
-              gameWord = ""; // reset gameword
-              letters = ""; // reset letters guessed
-              console.log("Amount right: " + String(amtRight));
-            } else {
-              // if not all letters have been revealed, add the guessed letter to the letter string
-              letters += keyString;
-              console.log("guessed letters: " + letters);
-              console.log("Amount right: " + amtRight);
+              $(".l" + String(i)).text(keyString); // write letter to the appropriate blank space
+              console.log(".l" + String(i));
+              amtRight++; // increase the amount correct
+              if (amtRight === gameWord.length) {
+                // if the amount of revealed letters matches the length of the gameWord, you win
+                $(".result").text("You win!");
+                gameWord = ""; // reset gameword
+                letters = ""; // reset letters guessed
+                console.log("Amount right: " + String(amtRight));
+                numWins++;
+                numGuess = 0;
+                wrongLet = "";
+                amtRight = 0;
+                $(".wins").text(numWins);
+                inWord = true;
+              } else {
+                // if not all letters have been revealed, add the guessed letter to the letter string
+                if (!inWord) {
+                  letters += keyString;
+                }
+                inWord = true;
+                console.log("guessed letters: " + letters);
+                console.log("Amount right: " + amtRight);
+              }
             }
           }
+          if (!inWord) {
+            numGuess++;
+            letters += keyString;
+            wrongLet += keyString;
+            $(".wrongRem").text(String(maxGuess - numGuess));
+            if (numGuess >= maxGuess) {
+              $(".result").text("You lose! :(");
+              gameWord = ""; // reset gameword
+              letters = ""; // reset letters guessed
+              numGuess = 0; // reset number of incorrect guesses
+              wrongLet = ""; // reset wrong letters
+              amtRight = 0; // reset number of right guesses
+            }
+          }
+          $(".guessed").text(wrongLet); // display wrong guesses
         }
       }
     }
   });
+
+  function inArray(checkKey, checkArr) {
+    var tflag = false;
+    for (var i = 0; i < checkArr.length; i++) {
+      //COMPARE TYPED VALUE TO LETTERS IN THE SECRET WORD
+      if (checkKey === checkArr[i]) {
+        // has the typed value already been guessed?
+        tflag = true; // flag for an already guessed letter
+      }
+    }
+    return tflag;
+  }
 });
